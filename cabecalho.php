e38fcd886369a240f215ea9e426d92b875a94ae5
edson-silva <81913175+edson-silva@users.noreply.github.com>
2022-08-26 20:58:07 -0300

    Add files via upload

---
+++ cabecalho.php
@@ -0,0 +1,54 @@
+<div id = "cabecalho_superior">
+
+<nav id = "menu_cima">
+
+      <ul>
+            <li><a href = "#"> Minha Conta </a> </li>
+            <li><a href = "#"> Meu Carrinho </a></li>
+            <li><a href = "#"> Logar </a></li>
+            <li><a href = "#"> Cadastrar </a></li>
+
+        </ul>
+
+</nav>
+
+</div>
+
+<div id = "cabecalho_meio">
+
+         <h1> Os melhores produtos estão aqui </h1>
+          <p> Nenhum item no seu carrinho de compras </p>
+
+<section>
+
+       <form action = "   ">
+
+           <label>
+
+                   <span> buscar  </span>
+                   <input type = "search" name = "pesquisa"  id= "pesquisa">
+                   <input 
+
+           </label>
+
+
+      </form>
+
+</section>
+
+
+
+
+
+
+</div>
+
+<div id = "cabecalho_inferior">
+
+
+
+
+
+
+
+</div>
---
+++ index.php
@@ -0,0 +1,57 @@
+<!Doctype html>
+
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+
+<title> Loja Virtual </title>
+<link href= "css/css.css" rel="stylesheet" />
+
+</head>
+
+<body>
+
+<div id = "Principal">
+
+    <section id="cabecalho">
+
+      <?php 
+      
+      include_once("cabecalho.php"); ?>
+
+
+
+    </section>  <!---Fim Topo --->
+
+    <section id = "corpo">
+
+
+
+
+
+
+
+
+    </section>  <!---fim corpo ---> 
+
+
+    <footer id = "rodape">
+
+
+
+
+
+
+    </footer> <!----Fim rodape ---->
+
+   
+
+</div> <!----Fim Principal --->
+
+
+</body>
+
+</html>
+
